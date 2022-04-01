5f8409801a0828d3ebb27354f53a1fa8405d82d6
Beran58 <96017201+Beran58@users.noreply.github.com>
2022-04-01 20:29:03 +0200

    Update LocalConfig.class.php
    Edited config - removed sensitice information

--- _includes/LocalConfig.class.php
+++ _includes/LocalConfig.class.php
@@ -1,9 +1,9 @@
 <?php
 class LocalConfig extends Config {
-    public const DBHOST = '127.0.0.1';
-    public const DATABASE = 'ip_3';
-    public const DBUSER = 'www-aplikace';
-    public const DBPASS = 'Bezpe4n0Heslo.';
+    public const DBHOST = ''; //Your DB host here.
+    public const DATABASE = ''; //Your DB name here.
+    public const DBUSER = ''; //Your DB username here.
+    public const DBPASS = ''; //Your DB password here.
 
-    public const DEBUG = true;
+    public const DEBUG = false;
 }
